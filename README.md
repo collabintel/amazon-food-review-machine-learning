8383d8171bfe9aab238c7b11e04b6183c8739242
Anil Derbent <anil.derbent@etpa.nl>
2019-11-03 02:35:48 +0100

    Implemented perceptron algorithm solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# amazon-food-review-perceptron
+# amazon-food-review-machine-learning
+
+This project is about creating a machine learning model from the amazon food-review dataset. Here is the link for the dataset: https://www.kaggle.com/snap/amazon-fine-food-reviews
---
+++ perceptron/tokenizer.py
@@ -0,0 +1,32 @@
+from string import punctuation, digits
+
+#pragma: coderesponse template
+def extract_words(input_string):
+    """
+    Helper function for bag_of_words()
+    Inputs a text string
+    Returns a list of lowercase words in the string.
+    Punctuation and digits are separated out into their own words.
+    """
+    for c in punctuation + digits:
+        input_string = input_string.replace(c, ' ' + c + ' ')
+
+    return input_string.lower().split()
+#pragma: coderesponse end
+
+
+#pragma: coderesponse template
+def bag_of_words(texts):
+    """
+    Inputs a list of string reviews
+    Returns a dictionary of unique unigrams occurring over the input
+    """
+    # Your code here
+    dictionary = {} # maps word to unique index
+    for text in texts:
+        word_list = extract_words(text)
+        for word in word_list:
+            if word not in dictionary:
+                dictionary[word] = len(dictionary)
+    return dictionary
+#pragma: coderesponse end
